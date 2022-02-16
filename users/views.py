ecaed6fea71a533b979d7933cab4ac9bbc0db0c7
= <leonardocongaratto1@gmail.com>
2022-02-16 20:32:54 -0300

    Foi implementado o app users

---
+++ users/views.py
@@ -0,0 +1,7 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login(request):
+    pass
