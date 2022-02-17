2df2e7f30630838f80811ab2d9a3cbebc3b30edd
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-02-17 17:55:29 -0300

    Esta sendo implementado método de authenticate

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from users.views import login
+from .views import authenticate, index
 
 urlpatterns = [
-    path('', login)
+    path('', authenticate),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,27 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.contrib.auth.models import User
 
-def login(request):
-    pass
+
+def authenticate(self, request, username=None, password=None):
+    login_valid = settings == username
+    pwd_valid = check_password(password == password)
+    if login_valid and pwd_valid:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = User(username=username)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        return None
+
+ 
+def get_user(self, user_id):
+    try:
+        return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return None
+
+
+def index(request):
+    return render(template_name='templates/login.html')
